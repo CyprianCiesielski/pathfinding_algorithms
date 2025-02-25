376e9cf09d16e5bc24568b3fc2fc9346cd860a3e
CyprianCiesielski <cyprianek2005@gmail.com>
2025-02-25 19:42:45 +0100

    DFS and BFS

---
+++ DFS_AND_BFS.py
@@ -0,0 +1,56 @@
+class Node():
+    def __init__(self, parent):
+        self.parent = parent
+    
+def DFS(graph, start, end):
+    stack = [(start, [start])]  
+    visited = set()
+
+    while stack:
+        node, path = stack.pop()
+
+        if node == end:
+            return path  
+        if node not in visited:
+            visited.add(node)
+
+            for neighbor in reversed(graph.get(node, [])):  
+                stack.append((neighbor, path + [neighbor]))
+                print((neighbor, path + [neighbor]))
+
+    return None
+
+
+
+
+def BFS(graph, start,end):
+    queue = [(start, [start])]  
+    visited = set()
+
+    while queue:
+        node, path = queue.pop(0)
+
+        if node == end:
+            return path  
+        if node not in visited:
+            visited.add(node)
+
+            for neighbor in reversed(graph.get(node, [])):  
+                queue.append((neighbor, path + [neighbor]))
+                print((neighbor, path + [neighbor]))
+
+    return None
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+start = 'A'
+end = 'F'
+
+print(BFS(graph, start, end))
